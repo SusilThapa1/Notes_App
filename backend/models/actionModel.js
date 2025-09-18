2ab1f0225ad76569b4c7c12dad3fdd9f48781e4f
SusilThapa1 <shrishthapaa@gmail.com>
2025-09-18 22:53:10 +0545

    added expiry time

--- backend/models/actionModel.js
+++ backend/models/actionModel.js
@@ -1,9 +1,13 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
-const actionSchema = new mongoose.Schema(
+const ActivityLogSchema = new mongoose.Schema(
   {
     user: {
-      _id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+      _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
       name: { type: String, required: true }, // store snapshot of username at action time
       role: { type: String, required: true }, // e.g., "admin", "editor", "user"
     },
@@ -11,7 +15,11 @@ const actionSchema = new mongoose.Schema(
       type: String,
       required: true, // e.g., "created_post", "deleted_user", "updated_profile"
     },
-    
+    details: {
+      type: mongoose.Schema.Types.Mixed, // flexible (object, string, etc.)
+      default: {},
+    },
+    status: { type: String, enum: ["Success", "Failed"], default: "SUCCESS" },
     ipAddress: {
       type: String, // optional: track from req.ip
     },
@@ -27,7 +35,9 @@ const actionSchema = new mongoose.Schema(
     versionKey: false,
   }
 );
+ActivityLogSchema.index(
+  { createdAt: 1 },
+  { expireAfterSeconds: 60 * 60 * 24 * 90 }
+);
 
-module.exports = mongoose.model("Action", actionSchema);
-
- 
+module.exports = mongoose.model("ActionLog", ActivityLogSchema);
