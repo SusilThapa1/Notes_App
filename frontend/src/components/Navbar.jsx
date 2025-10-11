8bc7af70a21f872071e50cad5a35e1cb297890ff
SusilThapa1 <shrishthapaa@gmail.com>
2025-10-11 23:24:53 +0545

    refactor:integrate reusable confirm dialogue box

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { FiAlertTriangle } from "react-icons/fi";
 import { MdLockOutline, MdLogout, MdOutlineDevices } from "react-icons/md";
 
 import { AuthContext } from "./Context/AuthContext";
-import Swal from "sweetalert2";
+import { showConfirm } from "../../Utils/alertHelper";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -20,25 +20,8 @@ const Navbar = () => {
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   const logout = async () => {
-    const response = await Swal.fire({
-      title: "Are you sure, you want to log out?",
-      text: "You have to use your credentials if you want to login again!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#49bb0f",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes",
-      cancelButtonText: "No",
-      background: "#E2E8F0",
-      scrollbarPadding: false,
-      customClass: {
-        popup: "text-base sm:text-lg md:text-xl",
-        title: "text-xl sm:text-2xl md:text-3xl font-semibold",
-        confirmButton:
-          "text-sm sm:text-base md:text-lg bg-blue-600 text-white px-4 py-2 rounded",
-        cancelButton:
-          "text-sm sm:text-base md:text-lg bg-gray-400 text-white px-4 py-2 rounded",
-      },
+    const response = await showConfirm({
+      text: "You will be logged out",
     });
 
     if (!response.isConfirmed) return;
@@ -152,11 +135,11 @@ const Navbar = () => {
                 userDetails?.profilepath ? "border-gray-300" : ""
               } relative z-30  shadow-lg`}
               src={
-                userDetails
+                userDetails && userDetails?.profilepath
                   ? `${
-                      import.meta.env.VITE_API_IMAGE_URL +
+                      import.meta.env.VITE_API_FILE_URL +
                       userDetails?.profilepath
-                    }` || "/prof.webp"
+                    }`
                   : "/prof.webp"
               }
               alt="profile"
