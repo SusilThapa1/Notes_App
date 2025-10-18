f382a91ddd801e1a91bf9413c39f5322b3fb9e92
SusilThapa1 <shrishthapaa@gmail.com>
2025-10-18 23:50:17 +0545

    fixed routes

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -2,15 +2,14 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Sidebar from "../components/Admin/Sidebar";
 import WelcomeAdmin from "../components/Admin/WelcomeAdmin";
 import ProgrammeManager from "../components/Admin/ProgrammeManager";
-import SemesterManager from "../components/Admin/SemesterManager";
 import ManageResources from "../components/Admin/ManageResources";
-import UploadResources from "../components/Admin/UploadResources";
 import Analytics from "../components/Admin/Analytics";
 import ManageUsers from "../components/Admin/ManageUsers";
+import UniversityManager from "../components/Admin/UniversityManager";
 
 const AdminPage = () => {
   return (
-    <div className="md:grid md:grid-cols-[20%,80%] gap-2 mt-[45px] overflow-hidden md:h-screen h-screen bg-transparent bg-no-repeat bg-center bg-cover scroll-smooth px-5 md:pl-0 ">
+    <div className="md:grid md:grid-cols-[20%,80%] gap-2 mt-[45px] overflow-hidden h-screen bg-transparent bg-no-repeat bg-center bg-cover scroll-smooth px-5 md:pl-0 ">
       <Sidebar />
       <Routes>
         {/* 👇 Redirect /study/admin to /study/admin/dashboard */}
@@ -20,8 +19,7 @@ const AdminPage = () => {
         <Route path="analytics" element={<Analytics />} />
         <Route path="manageusers" element={<ManageUsers />} />
         <Route path="manageprogramme" element={<ProgrammeManager />} />
-        <Route path="managesemester" element={<SemesterManager />} />
-        <Route path="uploadresources" element={<UploadResources />} />
+        <Route path="manageuniversities" element={<UniversityManager />} />
         <Route path="manageresources" element={<ManageResources />} />
       </Routes>
     </div>
