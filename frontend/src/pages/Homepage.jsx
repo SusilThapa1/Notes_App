1653a782a80cef488fd6712af08b30e444c65199
SusilThapa1 <shrishthapaa@gmail.com>
2025-10-19 22:15:00 +0545

    refactor: simplify Homepage component and remove dynamic background logic

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -8,24 +8,6 @@ const Homepage = () => {
   const location = useLocation();
   const existingReview = location.state?.review || null;
 
-  const bgImages = [
-    "https://plus.unsplash.com/premium_photo-1713296254777-0a89f05dcb60?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1930/background.webp",
-    "https://plus.unsplash.com/premium_photo-1683749808307-e5597ac69f1e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070/images/desktop.png",
-    "https://png.pngtree.com/thumb_back/fw800/background/20250728/pngtree-cozy-study-room-with-an-open-book-green-plants-and-a-image_17654821.webp",
-     
-  ];
-
-  const [bgIndex, setBgIndex] = useState(0);
-
-  // Cycle through background images every 5 seconds
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setBgIndex((prev) => (prev + 1) % bgImages.length);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div className="flex flex-col min-h-screen w-full overflow-x-hidden">
       {/* Overlay for readability */}
@@ -38,12 +20,10 @@ const Homepage = () => {
         {/* Hero / Welcome Section */}
         <section
           id="home"
-          className="parent relative flex flex-col gap-3 justify-center items-center h-[400px] mb-10 md:h-[600px] rounded-xl shadow-lg border border-lightGreen text-center text-white bg-cover bg-no-repeat bg-center transition-all duration-1000"
-          style={{
-            backgroundImage: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.1)), url(${bgImages[bgIndex]})`,
-          }}
+          className="relative flex flex-col gap-3 justify-center items-center h-[400px] mb-10 md:h-[600px] rounded-xl shadow-lg border border-lightGreen text-center text-white bg-cover bg-center bg-no-repeat animate-bg-slide"
         >
-          {/* Content */}
+          <div className="absolute inset-0 bg-black/20 rounded-xl"></div>
+
           <div className="relative z-10 flex flex-col gap-3 justify-center items-center">
             <h1 className="text-3xl md:text-5xl font-bold drop-shadow-lg">
               Learn, Explore & Excel
