8a5c23af68578fbb445f30829de78677afa78f29
SusilThapa1 <shrishthapaa@gmail.com>
2025-10-21 23:26:46 +0545

    refactor upload routes

--- backend/Routes/uploadRoutes.js
+++ backend/Routes/uploadRoutes.js
@@ -5,14 +5,15 @@ const {
   uploadUpdate,
   uploadList,
   uploadDelete,
+  getSingleUpload,
 } = require("../controllers/uploadController");
 const { verifyToken, verifyAdmin } = require("../middlewares/authMiddleware");
 const uploadRouter = express.Router();
 
-uploadRouter.post("/addData", verifyToken, verifyAdmin, uploadInsert);
-
-uploadRouter.get("/getUploadData", uploadList);
-uploadRouter.put("/updateData/:id", verifyToken, verifyAdmin, uploadUpdate);
-uploadRouter.delete("/deleteData/:id", verifyToken, verifyAdmin, uploadDelete);
+uploadRouter.post("/addData", verifyToken, uploadInsert);
+uploadRouter.get("/getUploadData", verifyToken, uploadList);
+uploadRouter.get("/getMyUploads", verifyToken, getSingleUpload);
+uploadRouter.put("/updateData/:id", verifyToken, uploadUpdate);
+uploadRouter.delete("/deleteData/:id", verifyToken, uploadDelete);
 
 module.exports = uploadRouter;
