7099db9e510cb1f9de55a87ff2c7fdd60ead0262
SusilThapa1 <shrishthapaa@gmail.com>
2025-10-26 21:04:03 +0545

    fixed dark and light mode colors

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,6 +1,5 @@
-// Homepage.js
 import { Link, useLocation } from "react-router-dom";
-import AboutUs from "../components/AboutUs";
+import AboutUs from "../components/Pages/AboutUs";
 import Review from "../components/Review/Review";
 
 const Homepage = () => {
@@ -11,14 +10,12 @@ const Homepage = () => {
     <div className="flex flex-col min-h-screen w-full overflow-x-hidden bg-light dark:bg-dark transition-colors duration-300">
       {/* Overlay for readability */}
       <div className="relative z-10 mt-20 px-5 md:px-10 lg:px-20 w-full">
-        <div className="animate-marqueeLong whitespace-nowrap text-sm md:text-base font-medium text-center text-gray-600 dark:text-gray-300 py-2 w-full">
+        <div className="animate-marqueeLong whitespace-nowrap text-sm md:text-base font-medium text-center text-subTextLight dark:text-subTextDark py-2 w-full">
           You can send feedback or any suggestions directly to :{" "}
-          <span className="text-blue-400 dark:text-blue-300">
-            eyeh193@gmail.com
-          </span>
+          <span className="text-editOutlineText">eyeh193@gmail.com</span>
         </div>
 
-        {/* Hero / Welcome Section */}
+        {/* Hero / Welcome resource */}
         <section
           id="home"
           className="relative flex flex-col gap-3 justify-center items-center h-[400px] mb-10 md:h-[600px] rounded-xl shadow-lg border-2 border-gray-100 dark:border-gray-600 text-center text-white bg-cover bg-center bg-no-repeat animate-bg-slide"
@@ -37,13 +34,13 @@ const Homepage = () => {
             <div className="flex gap-5">
               <a
                 href="#about"
-                className="mt-6 bg-transparent border-2 border-white text-white dark:border-gray-200 dark:text-gray-200 px-4 py-2 rounded-full font-semibold hover:bg-light hover:text-[#2b352b] dark:hover:bg-gray-200 dark:hover:text-gray-900 transition-all duration-300 shadow-lg"
+                className="mt-6 bg-transparent border-2 border-white text-white dark:border-textDark dark:text-textDark px-4 py-2 rounded-full font-semibold hover:bg-light hover:text-[#2b352b] dark:hover:bg-gray-200 dark:hover:text-textLight transition-all duration-300 shadow-lg"
               >
                 Explore
               </a>
               <Link
-                to="/study/contact-us"
-                className="mt-6 bg-transparent border-2 border-white text-white dark:border-gray-200 dark:text-gray-200 px-4 py-2 rounded-full font-semibold hover:bg-light hover:text-[#2b352b] dark:hover:bg-gray-200 dark:hover:text-gray-900 transition-all duration-300 shadow-lg"
+                to="/contact-us"
+                className="mt-6 bg-transparent border-2 border-white text-white dark:border-textDark dark:text-textDark px-4 py-2 rounded-full font-semibold hover:bg-light hover:text-[#2b352b] dark:hover:bg-gray-200 dark:hover:text-textLight transition-all duration-300 shadow-lg"
               >
                 Contact Us
               </Link>
@@ -53,20 +50,20 @@ const Homepage = () => {
 
         <hr className="w-full border-gray-300 dark:border-gray-600 border-[1px]" />
 
-        {/* About Us Section */}
+        {/* About Us resource */}
         <section
           id="about"
-          className="py-10 text-gray-800 dark:text-gray-200 relative z-10 scroll-m-10"
+          className="py-10 text-textLight dark:text-textDark relative z-10 scroll-m-10"
         >
           <AboutUs />
         </section>
 
         <hr className="w-full border-gray-300 dark:border-gray-600 border-[1px]" />
 
-        {/* Review Section */}
+        {/* Review resource */}
         <section
           id="review"
-          className="py-10 text-gray-800 dark:text-gray-200 relative z-10"
+          className="py-10 text-textLight dark:text-textDark relative z-10"
         >
           <Review existingReview={existingReview} />
         </section>
