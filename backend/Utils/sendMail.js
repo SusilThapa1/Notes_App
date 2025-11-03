bfb8998b49065115b254294496bf1ce66e2b593a
SusilThapa1 <shrishthapaa@gmail.com>
2025-11-03 23:07:32 +0545

    add centralized sendEmail utility for OTP emails

---
+++ backend/Utils/sendMail.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const { generateEmailTemplates } = require("../config/emailTemplates");
+const { transporter } = require("../config/nodeMailer");
+
+const sendEmail = async ({ to, type, otp }) => {
+  const { html, text } = generateEmailTemplates(type, otp);
+
+  const mailOptions = {
+    from: process.env.SENDER_EMAIL,
+    to,
+    subject: {
+      verifyEmail: "Email Verification OTP",
+      passwordReset: "Password Reset OTP",
+      emailChange: "Email Change Verification OTP",
+    }[type],
+    text,
+    html: html.replace("{{image}}", "cid:logo@easy"),
+    attachments: [
+      {
+        filename: "desktop.png",
+        path: path.join(__dirname, "../config/desktop.png"),
+        cid: "logo@easy",
+      },
+    ],
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
